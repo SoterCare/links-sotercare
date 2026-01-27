bcead4f96869d26c089427a24feaec9e6e7c9374
Daham Dissanayake <dahamdissanayake05@gmail.com>
2026-01-28 01:55:14 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
     title: "SoterCare - Links",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body suppressHydrationWarning>{children}</body>
+            <body suppressHydrationWarning>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
